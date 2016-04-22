4526f0e8b5c862cf31c1a7d8affbc47be03ec614
Asha Bhandare <asha.bhandare@silicus.com>
2016-04-22 09:15:47 +0530

    ResourceType Map

---
+++ Finder/Silicus.Finder.Entities/CommonDataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silicus.Finder.Entities
+{
+    class CommonDataBaseContext : DataContextBase, ICommonDataBaseContext
+    {
+        public CommonDataBaseContext(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+
+           public IQueryable<T> Query<T>() where T : class
+        {
+            return Set<T>().AsNoTracking();
+        }
+        
+    }
+}
