aea8316c0af783a5b618389ff9cb977f69bf4c5e
Asha Bhandare <asha.bhandare@silicus.com>
2016-05-11 13:01:27 +0530

    Added dependency injection in Encourage

---
+++ Encourage/Silicus.Encourage.Web/App_Start/LightInjectWebCommon.cs
@@ -0,0 +1,30 @@
+﻿using LightInject;
+using Silicus.Encourage.DAL;
+using Silicus.Encourage.DAL.Interfaces;
+using Silicus.Encourage.Web.App_Start;
+using System.Reflection;
+
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(LightInjectWebCommon), "CreateContainer")]
+
+namespace Silicus.Encourage.Web.App_Start
+{
+    public static class LightInjectWebCommon
+    {
+        public static IServiceContainer CreateContainer()
+        {
+            IServiceContainer container = new ServiceContainer();
+            container.Register<IServiceContainer, ServiceContainer>();
+            InitializeContainer(container);
+            container.RegisterControllers(Assembly.GetExecutingAssembly());
+            container.EnableMvc();
+            return container;
+        }
+
+        private static void InitializeContainer(IServiceContainer container)
+        {
+            container.Register<IDataContextFactory, DataContextFactory>();
+        }
+
+    }
+}
---
+++ Encourage/Silicus.Encourage.Web/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Silicus.Encourage.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+        // GET: Account/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Account/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
