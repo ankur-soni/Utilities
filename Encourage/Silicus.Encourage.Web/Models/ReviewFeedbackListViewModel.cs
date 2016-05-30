85c74f5addff744e655625f07ba4af83bfe46e6e
abhishekjadhav3017 <abhishekjadhav3017@gmail.com>
2016-05-30 15:21:52 +0530

    Review.cs added

---
+++ Encourage/Silicus.Encourage.Web/Models/ReviewFeedbackListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Silicus.Encourage.Web.Models
+{
+    public class ReviewFeedbackListViewModel
+    {
+        public int NominationId { get; set; }
+        public string DisplayName { get; set; }
+        public string NominationTime { get; set; }
+        public string AwardName { get; set; }
+        public string Intials { get; set; }
+        public int Credits { get; set; }
+    }
+}
