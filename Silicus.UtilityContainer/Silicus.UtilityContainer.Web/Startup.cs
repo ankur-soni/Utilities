8a6bd1d731cf364737c3a790ac191f8ea75c0f86
Amit-Khandelwal <amit.khandelwal@silicus.com>
2016-08-03 15:38:40 +0530

    Removed unused usings.

--- Silicus.UtilityContainer/Silicus.UtilityContainer.Web/Startup.cs
+++ Silicus.UtilityContainer/Silicus.UtilityContainer.Web/Startup.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 using Hangfire;
 using Silicus.UtilityContainer.Web.App_Start;
 using System.Configuration;
-using Hangfire.SqlServer;
-using Microsoft.Owin.Security;
+
 
 namespace Silicus.UtilityContainer.Web
 {
@@ -17,8 +13,6 @@ namespace Silicus.UtilityContainer.Web
           
             ConfigureAuth(app);
 
-            //GlobalConfiguration.Configuration.UseSqlServerStorage(@"Server = tcp:utilitycontainerdbserver.database.windows.net,1433; Data Source = utilitycontainerdbserver.database.windows.net; Persist Security Info = False; User ID = SilUtilSqlUser; Password = Pa55w0rd; Pooling = False; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
-            //Hangfire Configuration
             var connectionString = ConfigurationManager.ConnectionStrings["HangfireDbConnection"].ConnectionString;
             GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
             //GlobalConfiguration.Configuration.UseSqlServerStorage(@"Data Source=10.4.1.190\SQLExpress1;User Id=SilUtilSqlUser;Password=Pa55w0rd");
