874c3716d3ecef23c3a591a979b7dd218e5dd374
DBirthare <DBirthare@SILICUS512>
2016-08-08 15:01:38 +0530

    Sign in, Create and other modification

--- Silicus.Reusable/Silicus.Reusable.Models/Frameworx.cs
+++ Silicus.Reusable/Silicus.Reusable.Models/Frameworx.cs
@@ -1,10 +1,24 @@
-﻿namespace Silicus.FrameworxProject.Models
+﻿using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Silicus.FrameworxProject.Models
 {
     public class Frameworx
     {
         public int Id { get; set; }
+         
         public string Title { get; set; }
+
+        [Display(Name = "Html Description")]
+        [AllowHtml]
         public string HtmlDescription { get; set; }
+
+        [Display(Name = "Demo Link")]
+        public string DemoLink { get; set; }
+
+        [Display(Name = "Source Code Link")]
+        public string SourceCodeLink { get; set; }
 
         public int CategoryId { get; set; }
 
---
+++ Silicus.Reusable/Silicus.Reusable.Web/App_Start/Startup.Auth.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Configuration;
+using System.Globalization;
+
+
+
+namespace Silicus.Reusable.Web
+{
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    RedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error?message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+        }
+    }
+}
