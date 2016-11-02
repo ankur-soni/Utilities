aec6a7cce4688e001b719b9a6e8768626077be06
Navnath Jadhav <navnath.jadhav@silicus.com>
2016-11-02 11:58:09 +0530

    TestSuite: Model was missing

---
+++ Silicus.Ensure/Silicus.Ensure.Models/DataObjects/TestSuite.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Silicus.Ensure.Models.DataObjects
+{
+    public class TestSuite
+    {
+        [Key]
+        public int TestSuiteId { get; set; }
+
+        [Required(ErrorMessage = "Test-Suite Name is required!")]
+        [StringLength(50, ErrorMessage = "Test-Suite name length should be less than or equal to 50 characters.")]
+        [Display(Name = "TestSuite Name")]
+        public string TestSuiteName { get; set; }
+
+        [Required(ErrorMessage="Duration is required!")]
+        public Int32 Duration { get; set; }
+
+        [Required(ErrorMessage = "Position is required!")]
+        public Int32 Position { get; set; }
+
+        [Required(ErrorMessage = "Competency is required!")]
+        public Int32 Competency { get; set; }
+
+        [Required(ErrorMessage = "Primary Tag is required!")]
+        [StringLength(500, ErrorMessage = "Tag length should be less than or equal to 500 characters.")]
+        [Display(Name="Primary Skill Tags")]
+        public List<Tags> PrimaryTags { get; set; }
+        
+        [StringLength(200, ErrorMessage = "Description length should be less than or equal to 50 characters.")]
+        [Display(Name = "Secondary Skill Tags")]
+        public List<Tags> SecondaryTag { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        public List<Tags> TagList { get; set; }
+    }
+}
