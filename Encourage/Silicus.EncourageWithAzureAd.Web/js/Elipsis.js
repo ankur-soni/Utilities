14f580ab4d8ec1d550cc2ee10d3049427406b4de
gshevkari <ganesh.shevkari@silicus.com>
2016-12-16 14:58:21 +0530

    - Removed debuggers

--- Encourage/Silicus.EncourageWithAzureAd.Web/js/Elipsis.js
+++ Encourage/Silicus.EncourageWithAzureAd.Web/js/Elipsis.js
@@ -4,7 +4,7 @@ $(function () {
     $('.more').each(function () {
         var content = $(this).text();
         if (content.length > showChar) {
-            debugger;
+            
             var con = content.substr(0, showChar);
             var hcon = content.substr(showChar, content.length - showChar);
             var txt = con + '<div class="morecontent"><div style="width:100%;max-width:100%;display: none;word-wrap: break-word;">' + hcon + '</div><a href="" class="moretxt">' + hidetxt + '</a></div>';
@@ -12,7 +12,7 @@ $(function () {
         }
     });
     $(".moretxt").click(function () {
-        debugger;
+        
         if ($(this).hasClass("sample")) {
             $(this).removeClass("sample");
             $(this).text(hidetxt);
