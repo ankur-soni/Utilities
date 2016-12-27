ca297f6ef338cc487e9811f72c2f934f9e25ac2d
nilkanthkapale10@gmail.com <Google@123>
2016-12-27 16:34:42 +0530

    Set Up Panel

---
+++ Silicus.Ensure/Silicus.Ensure.Entities/EntityConfigurations/PanelMap.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using Silicus.Ensure.Models.DataObjects;
+
+namespace Silicus.Ensure.Entities.EntityConfigurations
+{
+    internal class PanelMap : EntityTypeConfiguration<Panel>
+    {
+        public PanelMap()
+        {
+            HasKey(o => o.PanelId);
+
+            Property(p => p.PanelId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            ToTable(TableSettings.Panel, TableSettings.DefaultSchema);
+
+        }
+    }
+}
