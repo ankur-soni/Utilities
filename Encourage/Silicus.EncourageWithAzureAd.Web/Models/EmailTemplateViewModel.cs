8e21dd67207b178b8ac5cd15f1f2a5aed4a0457c
silicusaurabh <saurabh.babhulgaonkar@silicus.com>
2016-12-28 17:55:00 +0530

    Added EmailEditorTemplate related files.

--- Encourage/Silicus.EncourageWithAzureAd.Web/Models/EmailTemplateViewModel.cs
+++ Encourage/Silicus.EncourageWithAzureAd.Web/Models/EmailTemplateViewModel.cs
@@ -10,8 +10,11 @@ namespace Silicus.EncourageWithAzureAd.Web.Models
         public EmailTemplateViewModel()
         {
             ProcessesViewModel = new List<ProcessesViewModel>();
+            EmailTemplateEditor = new EmailTemplateEditorViewModel();
         }
 
         public List<ProcessesViewModel> ProcessesViewModel { get; set; }
+
+        public EmailTemplateEditorViewModel EmailTemplateEditor { get; set; }
     }
 }
