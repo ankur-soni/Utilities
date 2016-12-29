2f23068d9437d58ff6fee534f05376522542c72f
silicusaurabh <saurabh.babhulgaonkar@silicus.com>
2016-12-29 17:39:16 +0530

    Email Template Related changes.

--- Encourage/Silicus.EncourageWithAzureAd.Web/Controllers/EmailController.cs
+++ Encourage/Silicus.EncourageWithAzureAd.Web/Controllers/EmailController.cs
@@ -2,12 +2,14 @@
 using Silicus.EncourageWithAzureAd.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Silicus.EncourageWithAzureAd.Web.Controllers
 {
+    [Authorize]
     public class EmailController : Controller
     {
         private readonly IEmailTemplateService _emailTemplateService;  
@@ -35,7 +37,35 @@ namespace Silicus.EncourageWithAzureAd.Web.Controllers
         {
             var templateOf = processName + "Template";
             var emailTemplate = _emailTemplateService.GetEmailTemplate(templateOf);
-            return null;
+            EmailTemplateEditorViewModel emailTemplateEditor = new EmailTemplateEditorViewModel();
+
+            emailTemplateEditor.EmailTemplate = emailTemplate.Template;
+
+            var allManagers = _emailTemplateService.GetAllManagers();
+
+            foreach (var manager in allManagers)
+            {
+                var user = new UserViewModel();
+                user.Email = manager.EmailAddress;
+                user.Name = manager.DisplayName;
+                user.UserId = manager.ID;
+
+                emailTemplateEditor.Users.Add(user);
+            }
+
+            return PartialView("~/Views/Email/Shared/_emailTemplateEditor.cshtml", emailTemplateEditor);
+        }
+
+        [HttpPost]
+        public string SendMailToManagers(List<string> managersList,string emailTemplate)
+        {
+            string emailBody = HttpUtility.HtmlDecode(emailTemplate);
+
+            string subject = ConfigurationManager.AppSettings["MailNominationSubject"];
+
+            var result = _emailTemplateService.SendEmail(managersList, emailBody, subject);
+
+            return result;
         }
     }
 }
