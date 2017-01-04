45384ec8fcb37c7f3a0e2b4fb9c1766b406428aa
silicusaurabh <saurabh.babhulgaonkar@silicus.com>
2017-01-04 21:15:37 +0530

    Added Filters for awards in Shortlisted nominations

---
+++ Encourage/Silicus.EncourageWithAzureAd.Web/Models/ShortlistedNominationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Silicus.EncourageWithAzureAd.Web.Models
+{
+    public class ShortlistedNominationViewModel
+    {
+        public ShortlistedNominationViewModel()
+        {
+            ReviewFeedbackList = new List<ReviewFeedbackListViewModel>();
+            Awards = new List<LockAwardViewModel>();
+        }
+
+        public List<ReviewFeedbackListViewModel> ReviewFeedbackList { get; set; }
+        public List<LockAwardViewModel> Awards { get; set; }
+    }
+}
