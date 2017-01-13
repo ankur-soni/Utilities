022c625031987846f53865f7eb3eb8eb45ed7832
Ravindra Patil <ravindra.patil@silicus.com>
2017-01-13 10:37:28 +0530

    Candidate login changes

--- Silicus.Ensure/Silicus.Ensure.Web/App_Start/RouteConfig.cs
+++ Silicus.Ensure/Silicus.Ensure.Web/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace Silicus.Ensure.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Account", action = "Login", id = UrlParameter.Optional}
+                defaults: new { controller = "Admin", action = "Candidates", id = UrlParameter.Optional }
                 );
         }
     }
