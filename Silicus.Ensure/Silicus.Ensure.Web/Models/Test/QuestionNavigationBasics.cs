a417f5bff7427d83b80d64c0a17a6c130a00a31a
Pratik Patil <pratik.patil@silicus.com>
2017-01-19 10:38:41 +0530

    View models for candidate and reviwer screens

---
+++ Silicus.Ensure/Silicus.Ensure.Web/Models/Test/QuestionNavigationBasics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Silicus.Ensure.Web.Models.Test
+{
+    public class QuestionNavigationBasics
+    {
+        public List<int> Viewed { get; set; }
+        public List<int> Answered { get; set; }
+        public List<int> QuestionIds { get; set; }
+    }
+}
