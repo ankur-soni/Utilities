55f690722c6e6a195fe8ed1a25a0d41458376213
Pratik Patil <pratik.patil@silicus.com>
2017-01-25 10:39:33 +0530

    Added QuestionNavigationBasics and QuestionDetailsViewModel

---
+++ Silicus.Ensure/Silicus.Ensure.Web/Models/Test/QuestionDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Silicus.Ensure.Web.Models.Test
+{
+    public class QuestionDetailsViewModel
+    {
+        public int QuestionId { get; set; }
+        public int? UserTestSuiteId { get; set; }
+        public int? UserTestDetailId { get; set; }
+        public string Answer { get; set; }
+        public int QuestionType { get; set; }
+    }
+}
