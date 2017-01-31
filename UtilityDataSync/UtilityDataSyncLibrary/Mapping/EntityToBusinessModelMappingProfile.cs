f126caf5c59efc792c214c28d2afd850047511f2
gshevkari <ganesh.shevkari@silicus.com>
2017-01-31 12:47:08 +0530

    - Added utility sync service & test

---
+++ UtilityDataSync/UtilityDataSyncLibrary/Mapping/EntityToBusinessModelMappingProfile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AutoMapper;
+
+namespace UtilityDataSyncLibrary.Mapping
+{
+    public class EntityToBusinessModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "EntityToBusinessModel"; }
+        }
+
+        protected override void Configure()
+        {
+            CreateMap<vwExt_Client, Client>();
+            CreateMap<vwExt_Company, Company>();
+            CreateMap<vwExt_Department, Department>();
+            CreateMap<vwExt_Engagement, Engagement>();
+            CreateMap<vwExt_EngagementRoles, EngagementRoles>();
+            CreateMap<vwExt_EngagementTaskTypes, EngagementTaskTypes>();
+            CreateMap<vwExt_EngagementType, EngagementType>();
+            CreateMap<vwExt_Location, Location>();
+            CreateMap<vwExt_Resource, Resource>();
+            CreateMap<vwExt_ResourceHistory, ResourceHistory>();
+            CreateMap<vwExt_ResourceSkillLevel, ResourceSkillLevel>();
+            CreateMap<vwExt_Skill, Skill>();
+            CreateMap<vwExt_Title, Title>();
+            CreateMap<vwExt_User, User>();
+            CreateMap<vwExt_ResourceType, ResourceType>();
+        }
+
+        public static object GetDefault(Type type)
+        {
+            if (type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+            return null;
+        }
+    }
+}
