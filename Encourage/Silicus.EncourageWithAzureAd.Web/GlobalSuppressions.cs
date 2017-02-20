084af9f86be960e9bfd946c03d2ed35b7dd45d4f
shailendrabirthare <shailendra.birthare@gmail.com>
2017-02-20 13:08:44 +0530

    Fixing SonarQube issue

---
+++ Encourage/Silicus.EncourageWithAzureAd.Web/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S134:Control flow statements \"if\", \"switch\", \"for\", \"foreach\", \"while\", \"do\"  and \"try\" should not be nested too deeply", Justification = "<Pending>", Scope = "member", Target = "~M:Silicus.EncourageWithAzureAd.Web.Controllers.NominationController.ReviewNominations~System.Web.Mvc.ActionResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S134:Control flow statements \"if\", \"switch\", \"for\", \"foreach\", \"while\", \"do\"  and \"try\" should not be nested too deeply", Justification = "<Pending>", Scope = "member", Target = "~M:Silicus.EncourageWithAzureAd.Web.Controllers.NominationController.SavedNomination(System.Int32)~System.Web.Mvc.ActionResult")]
