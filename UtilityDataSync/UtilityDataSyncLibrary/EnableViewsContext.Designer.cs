5bb9339d1e7c51ea1067e3932ebb6d7c6e5f1abd
gshevkari <ganesh.shevkari@silicus.com>
2017-03-14 12:34:36 +0530

    - Modified utility DB schema for syncing - Modified data sync service to sync only required tables

--- UtilityDataSync/UtilityDataSyncLibrary/EnableViewsContext.Designer.cs
+++ UtilityDataSync/UtilityDataSyncLibrary/EnableViewsContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\gshevkari\Documents\Visual Studio 2013\Projects\UtilityDataSync\UtilityDataSyncLibrary\EnableViewsContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\gshevkari\Source\Repos\Utilities\UtilityDataSync\UtilityDataSyncLibrary\EnableViewsContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
