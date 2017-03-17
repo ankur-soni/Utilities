02351192644b943e0d4cb5f95a4b1ce9a6f58b8a
psatve <prashant.satve@silicus.com>
2017-03-17 11:09:31 +0530

    Text corrections in candidate info and test summary details

--- Silicus.Ensure/Silicus.Ensure.Web/Models/Test/CandidateInfoViewModel.cs
+++ Silicus.Ensure/Silicus.Ensure.Web/Models/Test/CandidateInfoViewModel.cs
@@ -9,13 +9,13 @@ namespace Silicus.Ensure.Web.Models.Test
     public class CandidateInfoViewModel
     {
         public string Name { get; set; }
-        [Display(Name = "Requisition Id")]
+        [Display(Name = "Requisition id")]
         public string RequisitionId { get; set; }
-        [Display(Name = "Total Experience")]
+        [Display(Name = "Total experience")]
         [DisplayFormat(DataFormatString = "{0:n2}")]
         public decimal TotalExperience { get; set; }
         public string Position { get; set; }
-        [Display(Name = "Date of Birth")]
+        [Display(Name = "Date of birth")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DOB { get; set; }
         public int TotalExperienceInYear { get; set; }
