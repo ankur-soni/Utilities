1c1d4c1525653500f5023690c1ce62397edf40c8
shailendrabirthare <shailendra.birthare@gmail.com>
2017-04-06 18:05:37 +0530

    Added code specific error handling.

---
+++ Encourage/Silicus.EncourageWithAzureAd.Web/Controllers/ErrorController.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Silicus.EncourageWithAzureAd.Web.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        public ActionResult BadRequest()
+        {
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return View();
+        }
+
+        public ActionResult Unauthorized()
+        {
+            Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            return View();
+        }
+
+        public ActionResult Forbidden()
+        {
+            Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            return View();
+        }
+
+        public ActionResult PageNotFound()
+        {
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
+            return View();
+        }
+
+        [ExcludeFromCodeCoverage]
+        public ActionResult ServerError()
+        {
+            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return View("CustomError");
+        }
+
+        [ExcludeFromCodeCoverage]
+        public ActionResult CustomError()
+        {
+            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return View();
+        }
+    }
+}
