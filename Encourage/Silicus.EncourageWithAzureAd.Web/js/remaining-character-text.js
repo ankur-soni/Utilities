53f328b521157458d2267e84f6c963c39b502d1d
shailendrabirthare <shailendra.birthare@gmail.com>
2017-05-15 19:32:52 +0530

    Issue 16 - All text boxes should have the check for allowed character length.

---
+++ Encourage/Silicus.EncourageWithAzureAd.Web/js/remaining-character-text.js
@@ -0,0 +1,14 @@
+﻿
+function showRemainingCharacterTextMessage(element) {
+    $(element).next('div.textarea').show();
+    var textMax = parseInt($(element).attr('maxlength'));
+    var textLength = $("#"+$(element).attr("id")+"").val().length;
+    var textRemaining = textMax - textLength;
+    $(element).next('div.textarea').html(textRemaining + ' characters remaining');
+}
+
+function hideRemainingCharacterTextMessage(maxLength,id) {
+    $('div.textarea').hide();
+}
+
+
