89461602ecd5bd816d4f27937d066e3bedce92fb
shailendrabirthare <shailendra.birthare@gmail.com>
2017-07-10 19:20:30 +0530

    Unit testing Started

---
+++ Encourage/Tests/Silicus.Encourage.Web.Tests/AccountControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Silicus.Encourage.Web.Tests
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void SignIn()
+        {
+            var accountController = new AccountController();
+        }
+    }
+}
---
+++ Encourage/Tests/Silicus.Encourage.Web.Tests/EmailControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Silicus.Encourage.Web.Tests
+{
+    [TestClass]
+    public class EmailControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
